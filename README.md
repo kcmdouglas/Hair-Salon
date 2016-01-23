193e6f3684d26730e9466be3068b536ef7c2e642
Kassidy Douglas <kcmdouglas@gmail.com>
2016-01-22 16:03:40 -0800

    Add working code for update function, change README file.

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-#To-Do using Databases
-
-#### Revisiting a simple Java to-do list using databases
-#### January 18, 2016.
+#Hair Salon
+#### Database set-up for a hair salon
+#### January 21, 2016.
 
 #### By Kassidy Douglas
 
 ## Description
 
-This repository is a homework exercise to aid in learning the basics about using databases in relation to Java applications.
+This repository is a code review for week three of Java. In it, a stylist owner can add new stylists, new clients for specific stylists, as well as delete and update information.
 
 ## Setup/Installation Requirements
+
+* Be sure to have gradle, postgres, and psql installed.
+* Run a server in a terminal, using
+`postgres`
+* In a separate terminal, run:
+`psql`
+* In PSQL, run these commands:
+`CREATE DATABASE hair_salon;
+CREATE TABLE stylists (id serial PRIMARY KEY, name varchar);
+CREATE TABLE clients (id serial PRIMARY KEY, name varchar, stylist_id int);
+CREATE DATABASE hair_salon_test WITH TEMPLATE hair_salon;`
+
+* Download this repository
+* In a separate terminal, navigate to your project folder then run:
+`psql hair_salon < hair_salon.sql`
+* Finally, in the same terminal in which you are viewing your project folder, run the command:
+  `gradle run`
 
 
 
 ## Technologies Used
 
-Java, Spark, JUnit, Velocity, Bootstrap, FluentLenium
+Java, Spark, JUnit, Velocity, Bootstrap, FluentLenium, Postgres and Sql2o
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,6 +23,7 @@ public class App {
       model.put("template", "templates/home.vtl");
 
       model.put("stylists", Stylist.all());
+      model.put("clients", Client.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -36,7 +37,7 @@ public class App {
     }, new VelocityTemplateEngine());
 
 // POST actions for stylists:
-
+// Add a new stylist on home page:
     post("/stylist/new", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("stylist");
@@ -48,6 +49,7 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+// Delete a stylist on invididual stylist page:
     post("/stylist/:id/delete", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       Stylist stylist = Stylist.find(Integer.parseInt(request.params("id")));
@@ -57,13 +59,15 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+// Update a stylist name on individual stylist page:
     post("/stylist/:id", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String newName = request.queryParams("updateStylist");
       Stylist stylist = Stylist.find(Integer.parseInt(request.params("id")));
-      stylist.update("newName");
+      stylist.update(newName);
 
-      model.put("stylists", Stylist.all());
+      model.put("stylist", stylist);
+
       model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -85,19 +89,19 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // post("/stylist/:id/client/new", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   String name = request.queryParams("client");
-    //   Integer stylistId = Integer.parseInt(request.queryParams("stylistId"));
-    //
-    //   Client newClient = new Client(name, stylistId);
-    //
-    //   newClient.save();
-    //
-    //   model.put("stylists", Stylist.all());
-    //   model.put("template", "templates/stylist.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    post("/stylist/:id/client/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("client");
+      Integer stylistId = Integer.parseInt(request.params("id"));
+
+      Client newClient = new Client(name, stylistId);
+
+      newClient.save();
+
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
 
     post("/stylist/:id/client/delete", (request, response) -> {
@@ -109,7 +113,21 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-
+    // post("/client/update", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //   Client client = Client.find(Integer.parseInt(request.queryParams("clientId")));
+    //   Stylist stylist = Stylist.find(Integer.parseInt(request.queryParams("stylistId")));
+    //   Integer stylistId = Integer.parseInt(request.queryParams("stylistId"));
+    //   String name = request.queryParams("clientName");
+    //
+    //   client.update(name, stylistId);
+    //   model.put("client", client);
+    //   model.put("stylist", stylist);
+    //   model.put("clients", Client.all());
+    //   model.put("stylists", Stylist.all());
+    //   model.put("template", "templates/home.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
 
   }
 }
